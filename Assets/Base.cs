d6db7c473fbcc7ff56e11b6f7614e9c600b3d387
deadem <deadem@gmail.com>
2016-12-26 17:53:39 +0300

    initial commit

---
+++ Assets/Base.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Base : MonoBehaviour {
+	public Rigidbody Bot;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < 5; ++i) {
+			float angle = Random.value * Mathf.PI * 2;
+			Vector3 position = new Vector3 (Mathf.Cos (angle), Mathf.Sin (angle)) * 5f;
+			Rigidbody bot = (Rigidbody) Instantiate(Bot, transform.position + position, Quaternion.identity);
+			bot.gameObject.SetActive(true);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
